710f63911577f50067562eae7c4154c529ab9439
Pavel <pavel.sclifos.05@gmail.com>
2024-10-31 22:20:00 +0200

    Final maybe v2

--- resources/views/loads/index.blade.php
+++ resources/views/loads/index.blade.php
@@ -33,12 +33,12 @@
                             <select name="status"
                                 class="mt-1 w-full px-4 py-2 rounded-md border-gray-300 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
                                 <option value="">All Statuses</option>
-                                <option value="new" {{ request('status') === 'new' ? 'selected' : '' }}>New</option>
-                                <option value="dispatched" {{ request('status') === 'dispatched' ? 'selected' : '' }}>
+                                <option value="new" {{ request('status') === 'New' ? 'selected' : '' }}>New</option>
+                                <option value="dispatched" {{ request('status') === 'Dispatched' ? 'selected' : '' }}>
                                     Dispatched</option>
-                                <option value="in_transit" {{ request('status') === 'in_transit' ? 'selected' : '' }}>In
+                                <option value="in_transit" {{ request('status') === 'In Transit' ? 'selected' : '' }}>In
                                     Transit</option>
-                                <option value="delivered" {{ request('status') === 'delivered' ? 'selected' : '' }}>
+                                <option value="delivered" {{ request('status') === 'Delivered' ? 'selected' : '' }}>
                                     Delivered</option>
                             </select>
                         </div>
@@ -124,9 +124,9 @@
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <span
                                             class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full
-                                        @if ($load->status === 'new') bg-yellow-100 text-yellow-800
-                                        @elseif($load->status === 'dispatched') bg-blue-100 text-blue-800
-                                        @elseif($load->status === 'in_transit') bg-purple-100 text-purple-800
+                                        @if ($load->status === 'New') bg-yellow-100 text-yellow-800
+                                        @elseif($load->status === 'Dispatched') bg-blue-100 text-blue-800
+                                        @elseif($load->status === 'In Transit') bg-purple-100 text-purple-800
                                         @else bg-green-100 text-green-800 @endif">
                                             {{ ucfirst($load->status) }}
                                         </span>
